061f2a2254a4f41ec0e12005e50dd81da2e9736d
yoavniran <yoavniran@gmail.com>
2020-04-20 22:55:11 +0300

    build: cypress use sb port from env

--- cypress/support/visitStory.js
+++ cypress/support/visitStory.js
@@ -1,4 +1,7 @@
 Cypress.Commands.add("visitStory", (component, storyName) => {
     cy.log(`cmd.loadStory: component = ${component}, story = ${storyName}`);
-    cy.visit(`${Cypress.env("storybookUrl")}${Cypress.env("components")[component]}--${storyName}`);
+
+    const url = `${Cypress.env("storybookDomain")}:${Cypress.env("SB_PORT")}${Cypress.env("storybookPath")}`;
+
+    cy.visit(`${url}${Cypress.env("components")[component]}--${storyName}`);
 });

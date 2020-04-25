7f8239ea1d40c9f4df8bd8cfe8e5078a034f9ae4
yoavniran <yoavniran@gmail.com>
2020-04-25 16:24:09 +0300

    feat: added ts definition for Uploady (wip)

---
+++ packages/ui/uploady/types/index.test-d.tsx
@@ -0,0 +1,24 @@
+
+import * as React from "react";
+import Uploady from "./index";
+
+const MyApp: React.FC = () => <Uploady debug >
+    <div>test</div>
+</Uploady>;
+
+export default () => <MyApp/>;
+
+
+// import * as React from "react";
+// import {  } from "./index";
+// // import { expectError } from "tsd";
+//
+// function TestComponent() {
+//     return (
+//         <Uploady   />
+//     );
+// }
+//
+// TestComponent();
+//
+//

11edc8c90ec0bf827b3a1d2fd3cceaeafadd19e2
yoavniran <yoavniran@gmail.com>
2020-12-01 00:05:40 +0200

    fix:  simple state File object not available in ssr

--- packages/core/simple-state/src/utils.js
+++ packages/core/simple-state/src/utils.js
@@ -1,13 +1,13 @@
 // @flow
 import { PROXY_SYM } from "./consts";
-import { isPlainObject, isProduction } from "@rpldy/shared";
+import { isPlainObject, isProduction, hasWindow } from "@rpldy/shared";
 
 const isProxy = (obj: Object) =>
     !isProduction() && !!obj &&
     !!~Object.getOwnPropertySymbols(obj).indexOf(PROXY_SYM);
 
 //check if object is File or react-native file object (it wont by instanceof File in react-native)
-const isNativeFile = (obj: any) => obj instanceof File || (obj.name && obj.size && obj.uri);
+const isNativeFile = (obj: any) => (hasWindow() && obj instanceof File) || (obj.name && obj.size && obj.uri);
 
 const isProxiable = (obj: any) =>
     Array.isArray(obj) || (isPlainObject(obj) && !isNativeFile(obj));

3ce6b4392d75215b13790643f15fa3dd5dd9b11e
yoavniran <yoavniran@gmail.com>
2021-01-07 00:12:06 +0200

    fix: filesParamName in upload options isnt repsected

--- packages/core/uploader/src/utils.js
+++ packages/core/uploader/src/utils.js
@@ -1,7 +1,7 @@
 // @flow
 import { hasWindow, isProduction } from "@rpldy/shared";
 import { unwrap, isProxiable, isProxy } from "@rpldy/simple-state";
-import { DEFAULT_OPTIONS, DEFAULT_PARAM_NAME } from "./defaults";
+import { DEFAULT_OPTIONS } from "./defaults";
 
 import type { Destination } from "@rpldy/shared";
 import type { CreateOptions } from "./types";

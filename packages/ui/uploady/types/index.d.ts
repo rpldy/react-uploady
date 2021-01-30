86e352a7b84749a802d94b5d5a246ecec564bd5a
Alex <cheuk.alex@gmail.com>
2021-01-29 17:01:37 -0800

    fix: @rpldy/uploady missing useFileInput type export

--- packages/ui/uploady/types/index.d.ts
+++ packages/ui/uploady/types/index.d.ts
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { UploadyProps }  from "@rpldy/shared-ui";
+import type { InputRef } from "@rpldy/shared-ui";
 
 export * from "@rpldy/shared-ui";
 
 export const Uploady: React.ComponentType<UploadyProps>;
 
 export default Uploady;
+
+export const useFileInput: (fileInputRef: InputRef) => void;

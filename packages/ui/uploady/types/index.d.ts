cff864db7f45740e2c4cdba7fd4bccd84b4cc153
yoavniran <yoavniran@gmail.com>
2021-02-25 18:01:32 +0200

    fix: export file and batch states enums from Uploady

--- packages/ui/uploady/types/index.d.ts
+++ packages/ui/uploady/types/index.d.ts
@@ -4,6 +4,11 @@ import type { InputRef } from "@rpldy/shared-ui";
 
 export * from "@rpldy/shared-ui";
 
+export {
+    FILE_STATES,
+    BATCH_STATES,
+} from "@rpldy/shared";
+
 export const Uploady: React.ComponentType<UploadyProps>;
 
 export default Uploady;

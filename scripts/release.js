5049ea5597135e9c1d2ea70b6f73e93edf82b4a8
yoavniran <yoavniran@gmail.com>
2021-03-08 13:24:45 +0200

    chore: release script

--- scripts/release.js
+++ scripts/release.js
@@ -3,39 +3,38 @@ const { execSync, execFileSync } = require("child_process")
 chalk = require("chalk"),
     shell = require("shelljs");
 
+/**
+ * release script separates lerna version and lerna publish
+ * This way, versioning happens before build&bundle so these processes
+ * can work with the packages' bumped version
+ *
+ */
 const release = () => {
-
     const publishArgs = process.argv.slice(2)
         .join(" ");
 
-    console.log(chalk.gray(`___ Running Lerna Version`));
+    console.log(chalk.gray(`___ Running *Lerna Version*`));
     let result = shell.exec(`lerna version`, { stdio: "inherit" });
-    // let result = execSync("lerna version", { stdio: "inherit" });
-
-    console.log("!!!!!!!!! version result = ", result);
-
 
     if (!result.code) {
-        console.log(chalk.green(`___ Lerna Version finished successfully`));
-        console.log(chalk.gray(`___ Running Build & Bundle`));
+        console.log(chalk.green(`___ *Lerna Version* finished successfully`));
+        console.log(chalk.gray(`___ Running *Build & Bundle*`));
         result = shell.exec("yarn build; yarn bundle:prod;")
 
         if (!result.code) {
-            console.log(chalk.green(`___ Build & Bundle finished successfully`));
-            console.log(chalk.gray(`___ Running Lerna Version with args: ${publishArgs}`));
+            console.log(chalk.green(`___ *Build & Bundle* finished successfully`));
+            console.log(chalk.gray(`___ Running *Lerna Publish* with args: ${publishArgs}`));
 
-            // result = shell.exec(`lerna publish from-package ${publishArgs}`)
-
+            // result = shell.exec(`lerna publish from-package ${publishArgs}`, { stdio: "inherit" });
         } else {
-            console.log(chalk.red(`Build & Bundle failed !!! (${result.code})`));
+            console.log(chalk.red(`*Build & Bundle* failed !!! (${result.code})`));
         }
 
     } else {
-        console.log(chalk.red(`Lerna Version Error !!! (${result.code})`));
+        console.log(chalk.red(`*Lerna Version* failed !!! (${result.code})`));
     }
 
     return process.exit(result.code);
 };
-
 
 release();

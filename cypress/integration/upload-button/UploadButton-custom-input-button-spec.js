38550c1c2a7bbb7669d278829d2349d3b40b4b7e
yoavniran <yoavniran@gmail.com>
2021-05-11 17:07:36 +0300

    test: custom input with custom button

---
+++ cypress/integration/upload-button/UploadButton-custom-input-button-spec.js
@@ -0,0 +1,29 @@
+import intercept from "../intercept";
+import uploadFile from "../uploadFile";
+
+describe("With Custom File Input And Button", () => {
+    const fileName = "flower.jpg";
+
+    before(() => {
+        cy.visitStory("uploadButton", "with-custom-file-input-and-custom-button&knob-destination_Upload Destination=url&knob-upload url_Upload Destination=http://test.upload/url");
+    });
+
+    it("should change file input attribute", () => {
+        intercept();
+
+        cy.get("#select-input-type")
+            .select("dir");
+
+        uploadFile(fileName, () => {
+            cy.wait("@uploadReq")
+                .interceptFormData((formData) => {
+                    expect(formData["testFile"]).to.eq(fileName);
+
+                    cy.storyLog().assertLogPattern(/ITEM_FINISH/, { times: 1 });
+                });
+        });
+
+        cy.get("input[type='file']")
+            .should("have.attr", "webkitdirectory", "true");
+    });
+});

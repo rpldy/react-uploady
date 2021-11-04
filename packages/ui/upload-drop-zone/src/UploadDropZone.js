690612944d2306335e6d21c837c485045ccb4301
Yoav Niran <yoavniran@users.noreply.github.com>
2021-11-04 13:36:39 +0200

    fix: drop-zone flickers on drag over child elements
    test: add lifeEvents unit tests

--- packages/ui/upload-drop-zone/src/UploadDropZone.js
+++ packages/ui/upload-drop-zone/src/UploadDropZone.js
@@ -7,25 +7,29 @@ import type { UploadOptions } from "@rpldy/shared";
 import type { UploadDropZoneProps } from "./types";
 
 const UploadDropZone: React$AbstractComponent<UploadDropZoneProps, ?HTMLDivElement> = forwardRef<UploadDropZoneProps, ?HTMLDivElement>(
-    (props, ref) => {
+    ({
+         className,
+         id,
+         children,
+         onDragOverClassName,
+         dropHandler,
+         htmlDirContentParams,
+         extraProps,
+         ...uploadOptions
+     }, ref) => {
+        const { upload } = useUploadyContext();
         const containerRef = useRef(null);
+        const dragLeaveTrackerRef = useRef(false);
 
         useImperativeHandle<?HTMLDivElement>(ref, () => containerRef.current, []);
-
-        const { upload } = useUploadyContext();
-
-        const {
-            className, id, children, onDragOverClassName, dropHandler,
-            htmlDirContentParams,
-			extraProps,
-            ...uploadOptions
-        } = props;
 
         //using ref so upload can stay memoized
         const uploadOptionsRef = useRef<?UploadOptions>();
         uploadOptionsRef.current = uploadOptions;
 
         const handleEnd = useCallback(() => {
+            dragLeaveTrackerRef.current = false;
+
             if (containerRef.current && onDragOverClassName) {
                 containerRef.current.classList.remove(onDragOverClassName);
             }
@@ -44,24 +48,34 @@ const UploadDropZone: React$AbstractComponent<UploadDropZoneProps, ?HTMLDivEleme
                 });
         }, [upload, dropFileHandler, uploadOptionsRef]);
 
-        const onDragOver = useCallback((e) => {
-            e.preventDefault();
+        const onDragEnter = useCallback((e) => {
+            dragLeaveTrackerRef.current =
+                (!dragLeaveTrackerRef.current && e.target === containerRef.current);
 
             if (containerRef.current && onDragOverClassName) {
                 containerRef.current.classList.add(onDragOverClassName);
             }
         }, [onDragOverClassName, containerRef]);
 
+        const onDragOver = useCallback((e) => {
+            //must have drag over event handler with preventDefault for drop to work
+            e.preventDefault();
+        }, []);
+
         const onDrop = useCallback((e: SyntheticDragEvent<HTMLDivElement>) => {
             e.preventDefault();
             e.persist();
+
             handleEnd();
             handleDropUpload(e);
         }, [handleEnd, handleDropUpload]);
 
-        const onDragLeave = useCallback(() => {
-            handleEnd();
-        }, [handleEnd]);
+        const onDragLeave = useCallback((e) => {
+            if (dragLeaveTrackerRef.current &&
+                e.target === containerRef.current) {
+                handleEnd();
+            }
+        }, [handleEnd, containerRef]);
 
         const onDragEnd = useCallback((e) => {
             e.preventDefault();
@@ -74,6 +88,7 @@ const UploadDropZone: React$AbstractComponent<UploadDropZoneProps, ?HTMLDivEleme
             className={className}
             ref={containerRef}
             onDragOver={onDragOver}
+            onDragEnter={onDragEnter}
             onDrop={onDrop}
             onDragLeave={onDragLeave}
             onDragEnd={onDragEnd}

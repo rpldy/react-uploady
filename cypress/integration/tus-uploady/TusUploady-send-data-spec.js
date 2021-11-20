ef3bf5c66c7866232bc05fd6f66f6c517ab1f0f2
yoavniran <yoavniran@gmail.com>
2021-11-20 14:36:14 +0200

    test: fix breaking tests

--- cypress/integration/tus-uploady/TusUploady-send-data-spec.js
+++ cypress/integration/tus-uploady/TusUploady-send-data-spec.js
@@ -1,6 +1,6 @@
 import uploadFile from "../uploadFile";
 import intercept from "../intercept";
-import { WAIT_SHORT } from "../specWaitTimes";
+import { WAIT_LONG, WAIT_SHORT } from "../specWaitTimes";
 
 describe("TusUploady - Send Data", () => {
     const fileName = "flower.jpg";
@@ -29,27 +29,27 @@ describe("TusUploady - Send Data", () => {
             .as("fInput");
 
         uploadFile(fileName, () => {
-            cy.wait(WAIT_SHORT);
-            cy.storyLog().assertFileItemStartFinish(fileName, 1);
+            cy.wait(WAIT_LONG);
 
-            cy.wait("@createReq")
-                .then(({ request }) => {
-                    expect(request.headers["content-length"]).to.eq("200000");
+            cy.storyLog()
+                .assertFileItemStartFinish(fileName, 1)
+                .then((startFinishEvents) => {
+                    cy.wait("@createReq")
+                        .then(({ request }) => {
+                            expect(request.headers["content-length"]).to.eq("200000");
 
-                    expect(request.headers["upload-metadata"])
-                        .to.eq("foo YmFy");
+                            expect(request.headers["upload-metadata"])
+                                .to.eq("foo YmFy");
 
-                    cy.storyLog().assertFileItemStartFinish(fileName, 1)
-                        .then((events) => {
-                            expect(events.finish.args[1].uploadResponse.location).to.eq("http://test.tus.com/upload/123");
+                            expect(startFinishEvents.finish.args[1].uploadResponse.location).to.eq("http://test.tus.com/upload/123");
                         });
-                });
 
-            cy.wait("@patchReq")
-                .then(({ request }) => {
-                    const { headers } = request;
-                    expect(headers["upload-offset"]).to.eq("200000");
-                    expect(headers["content-type"]).to.eq("application/offset+octet-stream");
+                    cy.wait("@patchReq")
+                        .then(({ request }) => {
+                            const { headers } = request;
+                            expect(headers["upload-offset"]).to.eq("200000");
+                            expect(headers["content-type"]).to.eq("application/offset+octet-stream");
+                        });
                 });
         }, "#upload-button");
     });

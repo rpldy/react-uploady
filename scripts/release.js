c58b702323be6e2704d1158af5c5008d0947c7f3
yoavniran <yoavniran@gmail.com>
2022-02-01 18:19:42 +0200

    chore: improve release script

--- scripts/release.js
+++ scripts/release.js
@@ -19,10 +19,12 @@ const shellCommand = (command) => {
     const result = getSuccessResult();
 
     try {
-        execSync(command, { stdio: "inherit" });
+        execSync(command, { stdio: [0, 1, "pipe"] }); //"inherit" });
     } catch (ex) {
         console.log(ex);
-        result.code = 1;
+        console.log("status = ", ex.status)
+        result.code = ex.status || 1;
+        result.shellError = ex.stderr.toString();
     }
 
     return result;
@@ -36,7 +38,15 @@ const log = (color, msg) =>
 
 const getLastCode = (results) => results.slice(-1)[0]?.code;
 
-const getResult = (results, taskId) => results.find(({ id }) => id === taskId);
+const getResult = (results, taskId) => {
+    const isArray = Array.isArray(taskId);
+
+    const find = (t) => results.find(({ id }) => id === t);
+
+    const outputs = [].concat(taskId).map(find);
+
+    return isArray ? outputs : outputs[0];
+}
 
 const runTask = async (results, { id, title, task }) => {
     let result;
@@ -161,11 +171,47 @@ const TASKS = [
         },
     },
     {
+      id: "branch",
+      title: "Create Release+Version Branch",
+      task: async (results) => {
+          const { version } = getResult(results, "changelog");
+          const branch =`release-${version.replace(/\./g, "_")}`;
+
+          let result = run(`git checkout -b ${branch}`);
+
+          const isBranchAlreadyExists = !!result.code &&
+              result.shellError.includes(`A branch named '${branch}' already exists`);
+
+          if (!result.code || isBranchAlreadyExists) {
+              if (isBranchAlreadyExists) {
+                  log(chalk.gray, `branch ${branch} already exists`);
+                  result = run(`git checkout ${branch}`);
+
+                  if (!result.code) {
+                      result = run(`git fetch origin`);
+                  }
+              }
+
+              if (!result.code) {
+                  log(chalk.gray, `pushing branch ${branch} to origin`);
+                  result = run(`git push -u origin ${branch}`);
+
+                  if (!result.code) {
+                      log(chalk.gray, `returning to release branch`);
+                      result = run(`git checkout release`);
+                  }
+              }
+          }
+
+          return { code: result.code, branch };
+      },
+    },
+    {
         id: "pr",
         title: "Create Release+Version PR",
         task: async (results) => {
-            const { version } = getResult(results, "changelog");
-            const { repo } = getResult(results, "gh-release");
+            const [{ version }, { repo }, { branch }] =
+                getResult(results, ["changelog",  "gh-release", "branch"]);
 
             const ghClient = createGitHubClient({}, [
                 createPullRequest
@@ -183,7 +229,7 @@ const TASKS = [
                 repo: repo.name,
                 title: `chore: release v${version}`,
                 base: "master",
-                head: `release-${version.replace(/\./g, "_")}`,
+                head:  branch,
                 changes: [{
                     files: {},
                     commit: "commit new version"
@@ -219,4 +265,3 @@ const release = async () => {
 };
 
 release();
-

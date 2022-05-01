f04ba0427b9600ebd2399ff36582b415058b43a7
yoavniran <yoavniran@gmail.com>
2022-05-01 16:44:27 +0300

    chore: fix ts

--- packages/ui/upload-drop-zone/types/index.test-d.tsx
+++ packages/ui/upload-drop-zone/types/index.test-d.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import UploadDropZone from "./index";
 
 const TestDropZone: React.FC = () => {
-    const onDrop = React.useCallback((e) => e.dataTransfer.files, []);
+    // const [draggedFiles, setDraggedFiles] = React.useState<FileList | null | undefined>(null);
+
+    const onDrop = React.useCallback((e: DragEvent) => e.dataTransfer?.files || [], []);
     const overlayRef = React.useRef<HTMLSpanElement | null>(null);
 
     const checkRemoveDragOver = React.useCallback(
-        ({ target }) => target === overlayRef.current, []);
+        ({ target } : DragEvent) => target === overlayRef.current, []);
 
     return <UploadDropZone
         autoUpload

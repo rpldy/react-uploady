5e1a93170214f909ab88bb6932ec66b970ef5bb8
yoavniran <yoavniran@gmail.com>
2022-11-05 12:38:53 +0200

    feat: drop-zone improvments
    - provide getFiles to dropHandler
    - allow control of whether to handle drag (boolean or callback)

--- packages/ui/upload-drop-zone/types/index.d.ts
+++ packages/ui/upload-drop-zone/types/index.d.ts
@@ -3,9 +3,15 @@ import { UploadOptions } from "@rpldy/shared";
 
 type DropResult = FileList | unknown[]
 
-export type DropHandlerMethod = (e: DragEvent) => DropResult | Promise<DropResult>;
+export type GetFilesMethod = () => Promise<File[]>;
+
+export type DropHandlerMethod = (e: DragEvent, getFiles: GetFilesMethod) => DropResult | Promise<DropResult>;
 
 export type ShouldRemoveDragOverMethod = (e: DragEvent) => boolean;
+
+export type ShouldHandleDragMethod = (e: DragEvent) => boolean;
+
+export type ShouldHandleDrag = boolean | ShouldHandleDragMethod;
 
 export interface UploadDropZoneProps extends UploadOptions {
     className?: string;
@@ -14,6 +20,7 @@ export interface UploadDropZoneProps extends UploadOptions {
     dropHandler?: DropHandlerMethod;
     htmlDirContentParams?: Record<string, unknown>;
     shouldRemoveDragOver?: ShouldRemoveDragOverMethod;
+    shouldHandleDrag?: ShouldHandleDrag,
     extraProps?: Record<string, unknown>;
     children?: JSX.Element | JSX.Element[];
 }

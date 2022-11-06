d185e8909c6e9f31d4b33a5c61d470d128a242c8
yoavniran <yoavniran@gmail.com>
2022-11-06 18:40:24 +0200

    fix: add missing props to mock-sender MockOptions TS interface

--- packages/core/mock-sender/types/index.d.ts
+++ packages/core/mock-sender/types/index.d.ts
@@ -1,11 +1,14 @@
 import { SendMethod } from "@rpldy/sender";
 import { UploaderEnhancer } from "@rpldy/uploader";
+import type { IsSuccessfulCall } from "@rpldy/shared";
 
 export interface MockOptions {
     delay?: number;
     fileSize?: number;
     progressIntervals?: number[];
     response?: any;
+    responseStatus?: number;
+    isSuccessfulCall?: IsSuccessfulCall;
 }
 
 export type MockSender = {

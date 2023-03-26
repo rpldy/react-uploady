be76ec9091120405b5c3c318d6f2abedff0f42e0
yoavniran <yoavniran@gmail.com>
2023-03-26 17:05:04 +0300

    chore: build release gh workflow wip

--- .github/actions/versionLog/index.js
+++ .github/actions/versionLog/index.js
@@ -2,14 +2,18 @@ const core = require("@actions/core"),
     // github = require("@actions/github"),
     { extractChangelogNotesForCurrentVersion } = require("../../../scripts/extractChangeLogNotes");
 
-try {
-    console.log("about to retrieve version info from CHANGELOG");
-    const { versionLog } = await extractChangelogNotesForCurrentVersion();
+const extractVersionLog = async () => {
+    try {
+        console.log("about to retrieve version info from CHANGELOG");
+        const { versionLog } = await extractChangelogNotesForCurrentVersion();
 
-    console.log("Retrieved version log = ", versionLog);
-    //core.getInput('who-to-greet');
-    core.setOutput("versionLog", versionLog);
+        console.log("Retrieved version log = ", versionLog);
+        //core.getInput('who-to-greet');
+        core.setOutput("versionLog", versionLog);
 
-} catch (ex) {
-    core.setFailed(ex.message);
+    } catch (ex) {
+        core.setFailed(ex.message);
+    }
 }
+
+extractVersionLog()

9bca8b5bebb2e41daab4bc1d7e635202aa6a61ff
yoavniran <yoavniran@gmail.com>
2023-10-16 11:46:30 +0300

    test: migrate tests to vitest
    coverage success

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,57 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { flowPlugin, esbuildFlowPlugin } from "@bunchtogether/vite-plugin-flow";
+import { esbuildFlowPlugin } from "@bunchtogether/vite-plugin-flow";
 import babelPlugin from "vite-plugin-babel";
 import { getMatchingPackages } from "./scripts/lernaUtils";
 
 const createPackageAliases = () => {
     const packages = getMatchingPackages();
-    // console.log("CREATED ALIASES ", aliases);
     return packages.reduce((res, p) => {
-        //{ find:/^i18n\!(.*)/, replacement: '$1.js' }
         res.push({
             find: new RegExp(`^${p.name}$`),
             replacement: path.resolve(`./${p.location}/src/index.js`)
         });
-        // res[`^${p.name}$`] = path.resolve(`./${p.location}/src/index.js`);
-        // res[`^${p.name}\/(.*)`] = path.resolve(`./${p.location}/$1`);
+
         return res;
     }, []);
 };
 
 export default defineConfig({
     plugins: [
-        flowPlugin(),
         babelPlugin({
             //passing specific config because setup file breaks when using config file
             babelConfig: {
                 babelrc: false,
                 configFile: false,
                 plugins: ["@babel/plugin-proposal-export-default-from"],
-                presets: ["@babel/react"]
+                presets: [
+                    [
+                        "@babel/env",
+                        {
+                            "modules": false,
+                        },
+                    ],
+                    "@babel/react", "@babel/flow"]
             },
         }),
-        react()
     ],
     test: {
         environment: "jsdom",
         globals: true,
         setupFiles: "./test/vitest-setup.js",
         include: ["packages/**/*.test.js?(x)"],
+        coverage: {
+            provider: "istanbul",
+            thresholdAutoUpdate: true,
+            reporter: ["lcov", "json", "text", "html"],
+            lines: 99.95,
+            branches: 98.32,
+            functions: 100,
+            statements: 99.95,
+            perFile: false,
+            exclude: ["**/*.mock.*"]
+        },
     },
     optimizeDeps: {
         esbuildOptions: {

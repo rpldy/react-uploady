93c8495ac7e1966d908c27bbc9548ee01f4fcd78
yoavniran <yoavniran@gmail.com>
2023-10-18 00:05:53 +0300

    chore: downgrade to react 17

--- cypress/integration/upload-preview/UploadPreview-multi-crop-spec.js
+++ cypress/integration/upload-preview/UploadPreview-multi-crop-spec.js
@@ -1,7 +1,6 @@
 import { interceptWithDelay } from "../intercept";
 import { uploadFileTimes } from "../uploadFile";
 import { BATCH_ADD, ITEM_START, ITEM_FINISH } from "../../constants";
-import { WAIT_SHORT, WAIT_X_SHORT } from "../../constants";
 import { examineCroppedUploadReq, examineFullUploadRequest } from "./examineCroppedUploadReq";
 
 describe("UploadPreview - Multi Crop", () => {
@@ -19,7 +18,7 @@ describe("UploadPreview - Multi Crop", () => {
         interceptWithDelay(100);
 
         uploadFileTimes(fileName, () => {
-            cy.wait(WAIT_X_SHORT);
+            cy.waitExtraShort();
 
             cy.storyLog().assertLogPattern(BATCH_ADD);
             cy.storyLog().assertLogPattern(ITEM_START, { times: 0 });
@@ -51,7 +50,7 @@ describe("UploadPreview - Multi Crop", () => {
 
             cy.storyLog().assertLogPattern(ITEM_START, { times: 3 });
 
-            cy.wait(WAIT_SHORT);
+            cy.waitShort();
 
             cy.storyLog().assertLogPattern(ITEM_FINISH, { times: 3 });
         }, 3, "#upload-btn");
@@ -62,11 +61,11 @@ describe("UploadPreview - Multi Crop", () => {
         interceptWithDelay(100);
 
         uploadFileTimes(fileName, () => {
-            cy.wait(WAIT_X_SHORT);
+            cy.waitExtraShort();
 
             cy.get("img.preview-thumb").eq(1).click();
             cy.get("#save-crop-btn").click();
-            cy.wait(WAIT_SHORT);
+            cy.waitShort();
             cy.get("#upload-all-btn").click();
 
             examineFullUploadRequest(cy.wait("@uploadReq"), "flower.jpg");
@@ -76,10 +75,10 @@ describe("UploadPreview - Multi Crop", () => {
                 .should("have.length", 2);
 
             uploadFileTimes(fileName, () => {
-                cy.wait(WAIT_X_SHORT);
+                cy.waitExtraShort();
                 cy.get("img.preview-thumb").eq(2).click();
                 cy.get("#save-crop-btn").click();
-                cy.wait(WAIT_SHORT);
+                cy.waitShort();
                 cy.get("img.preview-thumb.cropped")
                     .eq(0).should("exist");
                 cy.get("#upload-all-btn").click();
@@ -87,7 +86,7 @@ describe("UploadPreview - Multi Crop", () => {
                 examineCroppedUploadReq(cy.wait("@uploadReq"), "flower.jpg");
                 examineFullUploadRequest(cy.wait("@uploadReq"), "flower2.jpg");
 
-                cy.wait(WAIT_SHORT);
+                cy.waitShort();
 
                 cy.storyLog().assertLogPattern(ITEM_START, { times: 4 });
                 cy.storyLog().assertLogPattern(ITEM_FINISH, { times: 4 });

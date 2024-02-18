d618024dbe3245b2ab282709b6a225d2540b021e
yoavniran <yoavniran@gmail.com>
2024-02-18 19:56:44 +0200

    chore: upgrade flow to latest (0.229)

---
+++ cypress/integration/upload-drop-zone/UploadDropZone-keep-on-child-spec.js
@@ -0,0 +1,32 @@
+describe("UploadDropZone - Keep Drag on Child", () => {
+    it("should not remove drag style on child element", () => {
+        cy.visitStory("uploadDropZone", "with-child-element");
+
+        cy.get("#upload-drop-zone")
+            .trigger("dragenter", { dataTransfer: { items: [ { kind: "file" } ]} });
+
+        cy.get(".drag-over #dnd-child")
+            .should("be.visible");
+
+        cy.get("#upload-drop-zone")
+            .trigger("dragleave");
+
+        cy.get(".drag-over #dnd-child")
+            .should("not.exist")
+
+        cy.get("#upload-drop-zone")
+            .trigger("dragenter", { dataTransfer: { items: [ { kind: "file" } ]} });
+
+        cy.get(".drag-over #dnd-child")
+            .should("be.visible");
+
+        cy.get("#upload-drop-zone")
+            .trigger("dragleave");
+
+        cy.get("#dnd-child")
+            .trigger("dragenter", { dataTransfer: { items: [ { kind: "file" } ]} });
+
+        cy.get(".drag-over #dnd-child")
+            .should("be.visible");
+    });
+});

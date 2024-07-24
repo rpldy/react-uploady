1bbac3fc526109f46739c2f5f598dea000447ac8
yoavniran <yoavniran@gmail.com>
2024-07-25 00:15:20 +0300

    chore: pr-verify flow in GH
    replace circle-ci

--- scripts/reportBundleSizeToGithub.mjs
+++ scripts/reportBundleSizeToGithub.mjs
@@ -2,15 +2,22 @@ import core from "@actions/core"
 
 const reportBundleSize = async (data) => {
     core.info("outputting bundle size report to github summary...")
-    core.debug(JSON.stringify(data));
+    core.debug("bundle size data: " + JSON.stringify(data));
+
+    const report = [
+        //headers
+        Object.keys(data[0])
+            .map((key) =>
+                ({ data: key, header: true })),
+        //rows
+        ...data.map((row) => Object.values(row))
+    ];
+
+    core.debug("Summary Table: " + JSON.stringify(report));
 
     await core.summary
         .addHeading("Bundle Size Report 📦")
-        .addTable([
-            ...Object.keys(data[0]).map((key) =>
-                ({ data: key, header: true })),
-            ...data.flatMap((row) => Object.values(row))
-        ])
+        .addTable(report)
         .write();
 };
 

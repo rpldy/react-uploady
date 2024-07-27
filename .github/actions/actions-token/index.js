6335ccffd1cc4f5dd97d330272eb05d4f402c02f
yoavniran <yoavniran@gmail.com>
2024-07-28 00:13:48 +0300

    chore: compare bundle size between master and pr

---
+++ .github/actions/actions-token/index.js
@@ -0,0 +1,13 @@
+
+const actionScript = ({ core }) =>{
+    const runtimeToken = process.env.ACTIONS_RUNTIME_TOKEN;
+    core.setOutput("runtimeToken", runtimeToken);
+
+    const resultsUrl = process.env.ACTIONS_RESULTS_URL
+    core.setOutput("resultsUrl", resultsUrl);
+
+    core.info("Got GH Actions Tokens and set into step output");
+};
+
+export default actionScript;
+

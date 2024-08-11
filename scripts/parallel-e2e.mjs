9369e2f6c0e5df9683938e64c370264a5e210f71
yoavniran <yoavniran@gmail.com>
2024-08-11 17:15:59 +0300

    chore: run cypress e2e using parallel threads

---
+++ scripts/parallel-e2e.mjs
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+// import path from "path";
+import Yargs from "yargs";
+import { glob } from "glob";
+import { execa } from "execa";
+import { logger } from "./utils.mjs";
+
+const argv = Yargs(process.argv.slice(2))
+    .option("threads", {
+        alias: "t",
+        type: "number",
+        description: "Number of threads"
+    })
+    .option("command", {
+        alias: "c",
+        type: "string",
+        description: "test command to run per thread"
+    })
+    .option("arguments", {
+        alias: "a",
+        type: "string",
+        description: "arguments to pass to the test command",
+    })
+    .parse();
+// .option('bail', {
+//     alias: 'b',
+//     type: 'boolean',
+//     description: 'Exit on first suite finishing with errors'
+// })
+
+console.log("ARGS ", argv)
+
+const options = {
+    threads: argv.threads,
+    command: argv.command,
+    args: argv.arguments,
+};
+
+logger.info(">>> running E2E in Parallel", JSON.stringify(options));
+
+const getSpecs = async () => {
+//find specs files under cypress/integration folder
+    const specs = await glob("cypress/integration/**/*-spec.js");
+    console.log("specs", specs);
+    return specs;
+};
+
+const runGroupInThread = async (group, index, cancelSignal) => {
+    const cmdStr = [
+        `PRLL_THREAD_INDX=${index + 1} PRLL_GROUP_SIZE=${group.length}`,
+        `${options.command}`,
+        `--spec=${group.join(",")}`,
+        `${options.args}`
+    ].join(" ");
+
+    logger.info(`about to run specs group: ${index} with command: ${cmdStr}`);
+
+    const result = await execa({
+        reject: false,
+        shell: true,
+        cancelSignal,
+        stdout: ["pipe", "inherit"],
+        verbose: (line, { message, ...info }) => {
+            if (["command", "output"].includes(info.type)) {
+                logger.info(`[p:${index}] ${message}`);
+            } else {
+                logger.verbose(`[p:${index}] ${message}`);
+            }
+        }
+    })`${cmdStr}`;
+
+    logger.info(`finished running specs group ${index}`, result);
+
+    const { failed, exitCode } = result;
+
+    return {
+        index,
+        success: !failed,
+        exitCode,
+    };
+};
+
+const runInParallel = async (specGroups) => {
+    const aborter = new AbortController();
+    const cancelSignal = aborter.signal;
+
+    const runs = specGroups.map((group, index) => {
+        return runGroupInThread(group, index, cancelSignal)
+            .then((result) => {
+                if (!result.success) {
+                    logger.warn(`group ${index} failed! cancelling other runs...`);
+                    //cancel all other runs in case of one failure (bail!)
+                    aborter.abort();
+                }
+
+                return result;
+            });
+    });
+
+    const results = await Promise.all(runs);
+
+    results.forEach((res, index) => {
+        console.log(`group ${index} finished with result: ${res.success} and exit code: ${res.exitCode}`)
+    });
+
+    return !results.find((res) => !res.success);
+};
+
+const getSpecsInGroups = (specs) => {
+    const specGroups = [];
+    const groupSize = Math.ceil(specs.length / options.threads);
+
+    while (specs.length) {
+        specGroups.push(specs.splice(0, groupSize));
+    }
+
+    return specGroups;
+};
+
+const runTests = async () => {
+    const specs = await getSpecs();
+
+    logger.info(`found ${specs.length} specs`);
+
+    //divide the array of specs into arrays that match in count to options.threads:
+    const specGroups = getSpecsInGroups(specs);
+
+    const success = await runInParallel(specGroups);
+
+    logger.info(`>>> finished! success = ${success}`);
+
+    if (!success) {
+        process.exit(1);
+    }
+    // execa({
+    //     stdout: "inherit",
+    //     stderr: "inherit",
+    // })`jrm cypress/results/results-combined.xml "cypress/results/*.xml"`
+}
+
+runTests();

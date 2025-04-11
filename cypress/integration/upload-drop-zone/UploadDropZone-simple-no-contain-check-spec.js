3d11f7e4ec93693e3d6fb1d5a39100133fc5d547
Yoav Niran <yoavniran@users.noreply.github.com>
2025-04-11 15:16:30 +0300

    feat: Drop zone - allow full control over drag handling (#844)
    * fix: drop-zone client sole control over should handle
    
    - added e2e spec
    
    * chore: update e2e weights file
    
    * test: add noContainCheckForDrag test
    
    ---------
    
    Co-authored-by: Uploady CI <ci@react-uploady.org>

---
+++ cypress/integration/upload-drop-zone/UploadDropZone-simple-no-contain-check-spec.js
@@ -0,0 +1,19 @@
+describe("UploadDropZone - No Contain Check", () => {
+    before(() => {
+        cy.visitStory("uploadDropZone", "with-aria-modal-overlay", );
+    });
+
+    it("should work with aria modal", () => {
+        cy.get("#aria-modal-modal")
+            .trigger("dragenter", { dataTransfer: { items: [ { kind: "file" } ] } });
+
+        cy.get(".dropIndicator")
+            .should("be.visible");
+
+        cy.get(".dropIndicator")
+            .trigger("dragleave");
+
+        cy.get(".dropIndicator")
+            .should("not.be.visible");
+    });
+});

788d40fc49ab7b33eb9bdc7c774df7b2a7c1dac1
Yoav Niran <yoavniran@users.noreply.github.com>
2025-04-18 16:18:49 +0300

    chore: upgrade sb to latest (#853)
    - bring back welcome page

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,23 +4,32 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
     stories: [
-        "../packages/**/*.stories.js",
-        // "./welcome.stories.mdx"
+        "./welcome.storydoc.mdx",
+        "../packages/**/*.stories.js"
     ],
-    addons: [path.resolve("./.storybook/uploadyPreset"), "@storybook/addon-links", {
-        name: "@storybook/addon-docs", //path.dirname(require.resolve("@storybook/addon-docs/package.json")),
-        options: { transcludeMarkdown: true },
-    }, {
-        name: "@storybook/addon-essentials",
-        options: {
-            actions: true,
-            backgrounds: false,
-            controls: true,
-            docs: false,
-            viewport: false,
-            // toolbars: false
-        }
-    }, "@storybook/addon-interactions", "@storybook/addon-webpack5-compiler-babel"],
+    addons: [
+        path.resolve("./.storybook/uploadyPreset"),
+        "@storybook/addon-links",
+        {
+            name: "@storybook/addon-docs", //path.dirname(require.resolve("@storybook/addon-docs/package.json")),
+            options: {
+                transcludeMarkdown: true
+            },
+        },
+        {
+            name: "@storybook/addon-essentials",
+            options: {
+                actions: true,
+                backgrounds: false,
+                controls: true,
+                docs: false,
+                viewport: false,
+                // toolbars: false
+            }
+        },
+        "@storybook/addon-interactions",
+        "@storybook/addon-webpack5-compiler-babel"
+    ],
     framework: {
         name: "@storybook/react-webpack5",
         options: {},

29a3a1a3a3122674162607c6986c2846c2c7e89f
yoavniran <yoavniran@gmail.com>
2025-04-19 18:26:08 +0300

    chore: upgrade dev deps
    - apply custom minimal "runtime" for flow enums

--- babel.config.js
+++ babel.config.js
@@ -27,7 +27,16 @@ const config =  {
     plugins: [
         "@babel/plugin-proposal-function-bind",
         "@babel/plugin-proposal-export-default-from",
-        "babel-plugin-transform-flow-enums",
+        ["babel-plugin-transform-flow-enums", {
+            //avoid using flow-enums-runtime, just return a frozen "enum" object
+            getRuntime: (t) => t.arrowFunctionExpression(
+                [t.identifier("enumObj")],
+                t.callExpression(
+                    t.memberExpression(t.identifier("Object"), t.identifier("freeze")),
+                    [t.identifier("enumObj")]
+                )
+            )
+        }],
         "babel-plugin-syntax-hermes-parser",
         //adding these here to stop sb build from breaking on loose mode issue :(
         "minify-dead-code-elimination",

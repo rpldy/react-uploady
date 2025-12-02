789fb14a6cf3ff6e3ae30b67f469347ca614b65a
Yoav Niran <yoavniran@users.noreply.github.com>
2025-12-02 12:27:21 +0200

    chore: remove bundle size reporting and migrate to Overweight for val… (#929)
    * chore: remove bundle size reporting and migrate to Overweight for validation
    
    - Deleted the bundle size report JSON file and associated GitHub action.
    - Updated the workflow to use Overweight
    for bundle size validation instead of the previous custom action.
    - Removed related scripts and dependencies for bundle size reporting.
    - Added instructions for running bundle size checks in CONTRIBUTING.md.
    
    * fix: update Overweight command
     to always use console reporter for bundle size validation
    
    * chore: fix bundle script

--- scripts/reportBundleSizeToGithub.mjs
+++
@@ -1,9 +0,0 @@
-import core from "@actions/core";
-
-const outputBundleSize = (data) => {
-    core.info("outputting bundle size report...");
-    const dataStr = JSON.stringify(data);
-    core.setOutput("BUNDLE_SIZE_REPORT_DATA", dataStr);
-};
-
-export default outputBundleSize;
